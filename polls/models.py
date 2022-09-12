df215bb228224d20f5e4c395e3240b9c139c7e92
vtecB16 <bartosz.winkiewicz@infotech.edu.pl>
2022-09-12 12:27:19 +0200

    Zadania

--- polls/models.py
+++ polls/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
 class Question(models.Model):
+
+    class QuestionType(models.TextChoices):
+        CLOSED = "Zamknięte"
+        OPEN = "Otwarte"
+
     question_text = models.CharField(max_length=200, null=True)
+    question_type = models.CharField(max_length=200, choices=QuestionType.choices, default=QuestionType.CLOSED)
     pub_date = models.DateTimeField("date published")
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+class Answer(models.Model):
+    answer_text = models.CharField(max_length=200, null=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
