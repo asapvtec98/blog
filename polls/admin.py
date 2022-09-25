21a0ac013043900ddb5a0031d3cd43e6c6a370f5
vtecB16 <bartosz.winkiewicz@infotech.edu.pl>
2022-09-25 15:56:09 +0200

    WWW 3

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Answer, Question, Choice
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("question_text","question_type","pub_date")
+    list_filter =  ("pub_date","question_type","question_text")
+    search_fields = ['question_text']
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ("question","choice_text","pub_date","votes")
+    list_filter =  ("pub_date",)
+    search_fields = ["choice_text","question__question_text"]
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ("question","answer_text")
+    list_filter = ("pub_date",)
+    search_fields = ["answer_text"]
+    def question_text(self, obj):
+        return obj.question.question_text
+
+
+
+
--- polls/models.py
+++ polls/models.py
@@ -1,20 +1,33 @@
 from django.db import models
 
 class Question(models.Model):
-
+    def __str__(self):
+        return self.question_text
     class QuestionType(models.TextChoices):
         CLOSED = "Zamknięte"
         OPEN = "Otwarte"
+        
 
     question_text = models.CharField(max_length=200, null=True)
     question_type = models.CharField(max_length=200, choices=QuestionType.choices, default=QuestionType.CLOSED)
-    pub_date = models.DateTimeField("date published")
+    pub_date = models.DateTimeField('date published')
+    
 
 class Choice(models.Model):
+    def __str__(self):
+        return self.choice_text
+    
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    pub_date = models.DateTimeField('date published')
+   
 
 class Answer(models.Model):
+    def __str__(self):
+        return self.answer_text
+
     answer_text = models.CharField(max_length=200, null=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField('date published')
+    
