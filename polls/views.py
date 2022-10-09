b1e2465233f1da0053b9fc0cd352e3626489814b
vtecB16 <bartosz.winkiewicz@infotech.edu.pl>
2022-10-09 15:40:55 +0200

    Zad 4

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from multiprocessing.connection import answer_challenge
+from tkinter import ANCHOR
+from django.shortcuts import render, get_object_or_404
+from .models import Question,Answer
 
-# Create your views here.
+def question_list(request):
+    questions = Question.objects.all()
+    return render(request, 'polls/question_list.html', {'questions':questions})
+
+def question_detail(request, question_id):
+   question = get_object_or_404(Question, pk=question_id)
+   return render(request, 'polls/question_detail.html', {'question': question})
